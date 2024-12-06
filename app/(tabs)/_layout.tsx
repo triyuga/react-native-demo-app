9666f22ac7deed0d3fb330ca4bc3b6abf40cb988
Tim Marwick <tim.marwick@pruple.telstra.com>
2024-12-06 17:11:10 +1000

    Add products screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="products"
+        options={{
+          title: 'Products',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="tag.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
